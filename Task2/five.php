b8cd47eef673042ec778c546329adeb2dc906a09
safoo-1995 <143965356+safoo-1995@users.noreply.github.com>
2024-07-10 06:44:20 +0300

    three  commit

--- Task2/five.php
+++ Task2/five.php
@@ -1,17 +1,17 @@
 <?php
 $tests = array(6, 4, 9, 3, 12, 8, 7);
-$sort = array();
-$n = $tests[0]; 
+$n = [0]; 
 
-for ($i = 0; $i < count($tests) - 1; $i++) {
-    if ($n > $tests[$i]) {
-        $sort[] = $tests[$i + 1];
-        $n = $tests[$i + 1]; 
+for ($i = 0; $i < count($tests) ; $i++) {
+    foreach ($tests as $value) {
+        if ($tests[$i] < $value) {
+            $n[] = $tests[$i]; 
+        }
     }
 }
 
 
-foreach ($sort as $value) {
-    echo "$value<br>";
+foreach ($n as $element) {
+    echo "$element<br>";
 }
 ?>
---
+++ Task3/form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="./FormHandle/login.php" method="Get">
+    <h1>login form</h1>
+        <label>First name</label>
+        <input name="first name" type="text"><br><br>
+        <label>Last name</label>
+        <input name="last name" type="text"><br><br>
+        <label>Email</label>
+        <input name="Email" type="email"><br><br>
+        <label>Adress</label>
+        <input  name="Adress" type="text"><br><br>
+        <button type="submit" value="login">login</button>
+    </form>
+</body>
+</html>
